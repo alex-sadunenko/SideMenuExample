d456a7cf413d864b64ba9f990dde2b5c0c4460d0
alexandr_sadunenko <a.sadunenko@gmail.com>
2020-09-04 19:26:05 +0300

    Initial Commit

---
+++ SideMenuExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenuExample
+//
+//  Created by Alex on 04.09.2020.
+//  Copyright © 2020 Alex Sadunenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
